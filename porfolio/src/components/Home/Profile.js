0def31c3febea70ceb1d51bac2a521a32227746b
ValoQuang <qtruongngoc95@gmail.com>
2021-12-10 00:23:01 +0200

    initialize app

---
+++ porfolio/src/components/Home/Profile.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const Profile = () => {
+    return (
+        <div className="profile-container">
+            <div className="profile-parent">
+                <div className="profile-details">
+                    <div className="colz">
+                    <a href="https://www.facebook.com/ngoc.quang.754/">
+                        <i className="fa-fa-facebook-square"></i>
+                    </a>
+                    <a href="https://www.google.com/">
+                        <i className="fa-fa-google-plus-square"></i>
+                    </a>
+                    <a href="https://www.instagram.com/">
+                        <i className="fa-fa-instagram"></i>
+                    </a>
+                    <a href="https://www.youtube.com/">
+                        <i className="fa-fa-youtube-square"></i>
+                    </a>
+                    <a href="https://twitter.com/">
+                        <i className="fa-fa-github-square"></i>
+                    </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
