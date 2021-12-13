255fda00d6382e73af6ce4d34a89c29e709f8859
Q.Iiot <45687913+ValoQuang@users.noreply.github.com>
2021-12-13 10:20:04 +0200

    starting footer

--- porfolio/src/App.js
+++ porfolio/src/App.js
@@ -1,10 +1,13 @@
 import Profile from './components/Home/Profile';
+import Footer from "./components/Footer/Footer"
+
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <Profile/>
+      <Footer/>
     </div>
   );
 }
---
+++ porfolio/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import "./Profile.css"
+
+export const Footer = () => {
+    return (
+        <div className='footer-container'>
+            <div className='footer-parent'>
+                <img src={required ("../../../assets/Home/shape-bg.png")} alt="background picture"/>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Footer
